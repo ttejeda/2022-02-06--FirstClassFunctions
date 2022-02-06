3f406c184bce16555c47b71063c4f63d2099beca
ttejeda <ttejeda@ucol.mx>
2022-02-06 13:06:25 -0600

    practice 2, part 2. Arrow functions ES6

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 //Función clásica
-function greet(){
-    console.log("Hola");
-}
+/*function greet(){
+    console.log(`Hola`);
+}*/
+
+//Función flecha
+let greet = () => console.log(`Hola`);
 greet();
 
 //La función recibe una función como parámetro
-function logGreeting(fn){
-    fn(); //Ejecuta la función, si se ejecuta no se reconoce a fn como una función
-}
+/*function logGreeting(fn){
+    fn(); //Ejecuta la función; si se trata de ejecutar un dato, no se reconoce a fn como una función
+}*/
+
+//Función flecha
+let logGreeting = fn => fn();
 logGreeting(greet);
 
 //Le declaramos a una variable una función
-let greetMe = function(){
-    console.log('Hello from the funtion expression');
-}
+/*let greetMe = function(){
+    console.log(`Hello from the function expression`);
+}*/
+
+//Función flecha
+let greetMe = () => console.log(`Hello from the function expression`);
 greetMe();
 
 //Le da la función greetMe para que se ejecute dentro de logGreeting
